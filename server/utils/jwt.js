a8ebc55aaec5dfd8b9016e999393f3c1ff29771b
Yarin Bukai <72891249+byarin90@users.noreply.github.com>
2023-06-06 14:33:23 +0300

    ok

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -4,7 +4,6 @@ import Jwt from "jsonwebtoken";
 // create a function to create token
 export const createToken = ({ _id, role }) => {
     //!create a token with the _id and role in the payload,Secret key to create a token
-
     const token = Jwt.sign({ _id, role }, secret.JWT_SECRET, { expiresIn: '1m' })
     return token;
 }
